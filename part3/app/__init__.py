906dc379e1cf8c25221dd738b376d554b85f4901
« Ephely » <ephely1@gmail.com>
2025-03-28 12:33:13 +0100

    All files updated

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,6 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from config import Config
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_api
+from app.api.v1.admin import api as admin_api
 
 db = SQLAlchemy()
 jwt = JWTManager()
@@ -23,16 +29,11 @@ def create_app():
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
 
-    from app.api.v1.users import api as users_ns
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.reviews import api as reviews_ns
-
-
-
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_api, path='app/api/v1/auth')
+    api.add_namespace(users_ns, path='app/api/v1/users')
+    api.add_namespace(places_ns, path='app/api/v1/places')
+    api.add_namespace(reviews_ns, path='app/api/v1/reviews')
+    api.add_namespace(amenities_ns, path='app/api/v1/amenities')
+    api.add_namespace(admin_api, path='app/api/v1/admin')
 
     return app
